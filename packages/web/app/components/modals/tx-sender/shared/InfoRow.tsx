00caaebaa2c0181955d0487b67a8661f5e798f2e
Drakkein <wiktor@furman.me>
2019-10-08 22:42:56 +0200

    Bank transfer copy changes

--- packages/web/app/components/modals/tx-sender/shared/InfoRow.tsx
+++ packages/web/app/components/modals/tx-sender/shared/InfoRow.tsx
@@ -3,29 +3,47 @@ import { isString } from "lodash";
 import * as React from "react";
 import { ListGroupItem } from "reactstrap";
 
-import { TDataTestId, XOR } from "../../../../types";
+import { TDataTestId } from "../../../../types";
 import { CopyToClipboardButton } from "../../../shared/CopyToClipboardButton";
 
 import * as styles from "./InfoRow.module.scss";
 
+type TInfoRowCopyProps = {};
+
 type TInfoRowCommonProps = {
   caption: React.ReactNode;
+  allowClipboardCopy?: boolean;
+  value: React.ReactNode | string;
+  clipboardCopyValue?: string;
 };
 
-type TInfoRowProps = XOR<
-  { allowClipboardCopy: true; value: string },
-  { allowClipboardCopy?: false; value: React.ReactNode }
->;
+const selectCopyValue = (
+  value: string | React.ReactNode,
+  clipboardCopyValue: string | undefined,
+) => {
+  if (clipboardCopyValue) {
+    return clipboardCopyValue;
+  }
+
+  if (isString(value)) {
+    return value;
+  }
+
+  throw new Error("Value should be string or clipboardCopyValue should be set");
+};
 
 export const InfoRow: React.FunctionComponent<
-  TInfoRowCommonProps & TInfoRowProps & TDataTestId
-> = ({ caption, value, allowClipboardCopy, "data-test-id": dataTestId }) => (
+  TInfoRowCommonProps & TInfoRowCopyProps & TDataTestId
+> = ({ caption, value, allowClipboardCopy, "data-test-id": dataTestId, clipboardCopyValue }) => (
   <ListGroupItem className={styles.infoRow}>
     <div className={styles.infoCell}>{caption}</div>
     <div className={styles.infoCell}>
       <span data-test-id={dataTestId}>{value}</span>
-      {allowClipboardCopy && isString(value) && (
-        <CopyToClipboardButton className={cn(styles.copyToClipboard, "ml-2")} value={value} />
+      {allowClipboardCopy && (
+        <CopyToClipboardButton
+          className={cn(styles.copyToClipboard, "ml-2")}
+          value={selectCopyValue(value, clipboardCopyValue)}
+        />
       )}
     </div>
   </ListGroupItem>
