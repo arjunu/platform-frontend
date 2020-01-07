4b231748f2fdb759957688a78e913fabe0f657b4
Drakkein <wiktor@furman.me>
2020-01-07 11:22:18 +0100

    Update config

--- packages/web/codechecks-e2e-vis-reg.ts
+++ packages/web/codechecks-e2e-vis-reg.ts
@@ -8,16 +8,16 @@ export async function main(): Promise<void> {
 
 async function visReg(): Promise<void> {
   const execOptions = { timeout: 300000, cwd: process.cwd(), log: true };
-  await codechecks.saveCollection(
+  await codechecks.saveDirectory(
     "e2e-vis-reg",
     join(__dirname, "packages/web/cypress/screenshots"),
   );
 
   if (codechecks.isPr()) {
-    await codechecks.getCollection("e2e-vis-reg", join(__dirname, ".reg/expected"));
+    await codechecks.getDirectory("e2e-vis-reg", join(__dirname, ".reg/expected"));
     await exec("./node_modules/.bin/reg-suit compare", execOptions);
 
-    await codechecks.saveCollection("e2e-vis-reg-report", join(__dirname, ".reg"));
+    await codechecks.getDirectory("e2e-vis-reg-report", join(__dirname, ".reg"));
 
     const reportData = require("./.reg/out.json");
     await codechecks.success({
